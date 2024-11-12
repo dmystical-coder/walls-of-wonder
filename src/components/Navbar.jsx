54d1ce865df1775e4acb62ba08432aae7fc5990a
dmystical-coder <dmystical.coder@gmail.com>
2024-11-12 20:30:24 +0100

    initialize React project with Vite, add main components and configuration files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#"><img src="./assets/images/header-logo.svg"
+                        alt="Wall of Wonder. Click for home." /></a>
+                </li>
+                <li><a href="#collections">Collections</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Donate</a></li>
+            </ul>
+        </nav>
+    )
+}
